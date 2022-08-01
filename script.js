773d092f0c84e1411b30d861d505a686cd88a184
Anatoly Ryazanov <Ryazanov.AO@yandex.ru>
2022-08-01 17:09:22 +0500

    refactoring + optimization

--- script.js
+++ script.js
@@ -1,49 +1,99 @@
+/* Открытие\закрытие гамбургер меню */
 const mainMenu = document.querySelector('.header__list');
 const mainHamburger = document.querySelector('.hamburger');
-const menuLinks = document.querySelectorAll('.header__link');
-const writeMe = document.querySelector('.contact__button');
-const modalWindow = document.querySelector('.modal');
-const contactSubmit = document.querySelector('.btn__submit');
-const userName = document.getElementById('userName');
-const modalInputs = document.querySelectorAll('.user__field')
 
-/* преобразование гампургер меню */
-mainHamburger.addEventListener('click', () => {
-    mainMenu.classList.toggle('header__list--active');
-    mainHamburger.classList.toggle('hamburger--active');
-});
+let isOpenMenu = false;
 
-/* плавный скролл и закрытие меню по клику на ссылку */
-for (const menuLink of menuLinks) {
-    menuLink.addEventListener('click', (e) => {
-        e.preventDefault();
-        let scrollToElem = menuLink.getAttribute('href');
-        let coordinates = document.querySelector(scrollToElem).offsetTop;
-        window.scrollTo({
-            top: coordinates - 100,
-            behavior: 'smooth'
-        });
+function showMenu() {
+    if(!isOpenMenu &&
+        mainMenu &&
+        mainHamburger
+    ) {
+        mainMenu.classList.add('header__list--active');
+        mainHamburger.classList.add('hamburger--active');
+        isOpenMenu = true;
+    } else if (
+        isOpenMenu &&
+        mainMenu &&
+        mainHamburger
+    ) {
         mainMenu.classList.remove('header__list--active');
         mainHamburger.classList.remove('hamburger--active');
-    })
+        isOpenMenu = false;
+    }
+}
+
+mainHamburger.onclick = showMenu;
+
+/* По клику на ссылку плавный скролл и закрытие меню  */
+const menuLinks = document.querySelectorAll('.header__link');
+
+function smoothScrollToLink(event, link, px) {
+    if(link) {
+        event.preventDefault();
+        let scrollToElem = link.getAttribute('href');
+        let coordinates = document.querySelector(scrollToElem).offsetTop;
+        window.scrollTo({
+            top: coordinates - px,
+            behavior: 'smooth'
+        });
+    }
 };
 
-/* открытие модального окна, закрытие по клику на бэкграунд и крестик */
-writeMe.addEventListener('click', () => {
-    modalWindow.classList.toggle('modal--active');
-    userName.focus();
-
+menuLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        smoothScrollToLink(event, link, 100);
+        showMenu();
+    })
 })
 
-modalWindow.addEventListener('click', (event) => {
-    if (
-        event.target.matches('.btn__close') || 
-        !event.target.closest('.modal__wrap')
+/* открытие модального окна, закрытие по клику на бэкграунд и крестик */
+const writeMe = document.querySelector('.contact__button');
+const modalWindow = document.querySelector('.modal');
+const userName = document.getElementById('userName');
+const modalInputs = document.querySelectorAll('.user__field');
+
+let isOpenModal = false;
+
+function showModal(modal, input) {
+    if(
+        !isOpenModal && 
+        modal &&
+        input
     ) {
-        modalWindow.classList.remove('modal--active');
-        for (modalInput of modalInputs) {
-            modalInput.value = '';
-          };
+        modal.classList.add('modal--active');
+        input.focus();
+        isOpenModal = true;
     }
+};
+
+function clearInputs(inputs) {
+    if(inputs) {
+        inputs.forEach(element => {
+            element.value = '';
+        });
+    }
+};
+
+function closeModal(event, modal, classCloseButton, classModalForm) {
+    if (
+        isOpenModal &&
+        modal &&
+        (event.target.matches(classCloseButton) || 
+        !event.target.closest(classModalForm))
+        ) {
+            modal.classList.remove('modal--active');
+            isOpenModal = false;
+        }
+};
+    
+writeMe.addEventListener('click', () => {
+    showModal(modalWindow, userName);
 });
 
+modalWindow.addEventListener('click', (event) => {
+    closeModal(event, modalWindow, '.btn__close', '.modal__wrap', );
+    if(!isOpenModal) {
+        clearInputs(modalInputs);
+    }
+});
